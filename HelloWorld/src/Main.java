3a3fed477260d51f758752226f79a6e5b69bf406
sze9mikatt <sze9mikatt@taniroda-hp-12.vasvari.hu>
2020-09-12 14:07:07 +0200

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		/*System.out.println("Hello World"); //syso ctrl+space
+		System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+		*/
+		
+		System.out.println("Vezetéknév: "+ args[0]);
+		System.out.println("Keresztnév: "+ args[1]);
+		System.out.println("Életkor: "+ args[2]);
+		
+		
+	}
+	
+
+}
